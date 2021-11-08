b758f14f950bb6133e5b050de9cb4d11507bb0ca
Beginneraboutlife116 <pythonforeye@gmail.com>
2021-11-09 03:02:02 +0800

    feat/increase UserTop component and finish data binding

---
+++ src/components/UserTop.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="col-3">
+    <a href="#">
+      <img
+        :src="user.image || 'http://via.placeholder.com/300x300?text=No+Image'" 
+        width="140px"
+        height="140px"
+      >
+    </a>
+    <h2> {{ user.name }} </h2>
+    <span class="badge bg-secondary">追蹤人數： {{ user.FollowerCount }} </span>
+    <p class="mt-3">
+      <button
+        v-if="user.isFollowed"
+        type="button"
+        class="btn btn-danger"
+        @click.stop.prevent="cancelFollowUser"
+      >
+        取消追蹤
+      </button>
+      <button
+        v-else
+        type="button"
+        class="btn btn-primary"
+        @click.stop.prevent="followUser"
+      >
+        追蹤
+      </button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserTop',
+  props: {
+    initialUser: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      user: {...this.initialUser}
+    }
+  },
+  methods: {
+    followUser() {
+      this.user.isFollowed = true
+    },
+    cancelFollowUser() {
+      this.user.isFollowed = false
+    }
+  }
+}
+</script>
--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -4,14 +4,84 @@
     <h1 class="mt-5">
       美食達人
     </h1>
+    <hr>
+    <div class="row text-center">
+      <!-- 美食達人列表 -->
+      <UserTop
+        v-for="user of users"
+        :key="user.id"
+        :initial-user="user"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import NavTabs from './../components/NavTabs.vue'
+import UserTop from './../components/UserTop.vue'
+
+const dummyData = {
+  "users": [
+    {
+      "id": 1,
+      "name": "root",
+      "email": "root@example.com",
+      "password": "$2a$10$6RaicCQtuc3V1fqOV6kRXeVUlRtinNPqGIfdIzxk8ssx3F.4i8pJm",
+      "isAdmin": true,
+      "image": null,
+      "createdAt": "2021-11-05T03:55:21.000Z",
+      "updatedAt": "2021-11-05T03:55:21.000Z",
+      "Followers": [],
+      "FollowerCount": 0,
+      "isFollowed": false
+    },
+    {
+      "id": 2,
+      "name": "user1",
+      "email": "user1@example.com",
+      "password": "$2a$10$s/g.WV6HuLIpeBVCPx/nm.76XI31zEBXdmOsqQFtM1s/ISJbzWW7m",
+      "isAdmin": false,
+      "image": null,
+      "createdAt": "2021-11-05T03:55:21.000Z",
+      "updatedAt": "2021-11-05T03:55:21.000Z",
+      "Followers": [],
+      "FollowerCount": 0,
+      "isFollowed": false
+    },
+    {
+      "id": 3,
+      "name": "user2",
+      "email": "user2@example.com",
+      "password": "$2a$10$ocEpcuzrgpC.PW6ID7Rf2.4iL/Wmnc6dGsCm7ipAyBlpNJaWgHz5K",
+      "isAdmin": false,
+      "image": null,
+      "createdAt": "2021-11-05T03:55:21.000Z",
+      "updatedAt": "2021-11-05T03:55:21.000Z",
+      "Followers": [],
+      "FollowerCount": 0,
+      "isFollowed": false
+    }
+  ]
+}
+
 export default {
+  name: 'UsersTop',
   components: {
-    NavTabs
+    NavTabs,
+    UserTop
+  },
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    this.fetchUsers()
+  },
+  methods: {
+    fetchUsers() {
+      this.users = [ ...dummyData.users ]
+    }
   }
 }
 </script>
